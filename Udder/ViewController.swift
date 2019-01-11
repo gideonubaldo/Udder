8fc308b10733470d8901923550880947a632d2dd
Gideon <gideon.ubaldo@sjsu.edu>
2019-01-10 17:52:43 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Gideon Ubaldo on 1/10/19.
+//  Copyright © 2019 Gideon Ubaldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
